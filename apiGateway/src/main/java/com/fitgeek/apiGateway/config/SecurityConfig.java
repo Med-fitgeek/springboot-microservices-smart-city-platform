5020966edc12fa5ada293c1772b9082ca1b3f2ba
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-04 17:28:12 +0200

    Fix : Updated apiGateway configuration file

--- apiGateway/src/main/java/com/fitgeek/apiGateway/config/SecurityConfig.java
+++ apiGateway/src/main/java/com/fitgeek/apiGateway/config/SecurityConfig.java
@@ -1,2 +1,27 @@
-package com.fitgeek.apiGateway.config;public class SecurityConfig {
+package com.fitgeek.apiGateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/health").permitAll()
+                        .anyExchange().authenticated()
+
+                )
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
