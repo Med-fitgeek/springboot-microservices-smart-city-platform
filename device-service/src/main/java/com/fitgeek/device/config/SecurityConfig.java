6cc66115d89089cd44d3866fa4f3c33a804d82fc
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-04 20:33:11 +0200

    feat : added device service SecurityConfig ad JwtRoleConverter classes

---
+++ device-service/src/main/java/com/fitgeek/device/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.fitgeek.device.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(
+            HttpSecurity http, JwtAuthenticationConverter jwtAuthenticationConverter) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/actuator/health")
+                        .permitAll()
+
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(
+                                        jwtAuthenticationConverter)))
+
+                .build();
+    }
+
+}
