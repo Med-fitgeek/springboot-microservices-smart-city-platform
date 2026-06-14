84f5519cd887a02c99a19a30da5fe05cb0db8b44
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-14 19:22:16 +0200

    feat : monitoring-service: created MonitoredDeviceController

---
+++ monitoring-service/src/main/java/com/fitgeek/monitoring/controller/MonitoredDeviceController.java
@@ -0,0 +1,42 @@
+package com.fitgeek.monitoring.controller;
+
+import com.fitgeek.monitoring.dto.MonitoredDeviceResponse;
+import com.fitgeek.monitoring.service.MonitoredDeviceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/monitoring/devices")
+@RequiredArgsConstructor
+public class MonitoredDeviceController {
+
+    private final MonitoredDeviceService service;
+
+    @GetMapping
+    public ResponseEntity<Page<MonitoredDeviceResponse>>
+    getDevices(
+            @PageableDefault(size = 20)
+            Pageable pageable) {
+
+        return ResponseEntity.ok(
+                service.getMonitoredDevices(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MonitoredDeviceResponse>
+    getDevice(@PathVariable UUID id) {
+
+        return ResponseEntity.ok(
+                service.getMonitoredDeviceById(id));
+    }
+}
