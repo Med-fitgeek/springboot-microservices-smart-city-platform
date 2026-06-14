f076832ab0fd8b1541c168099136e51e895154cd
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-14 21:28:32 +0200

    feat : alert-service: created AlertSeverity and AlertType enums

---
+++ alert-service/src/main/java/com/fitgeek/alert/entity/enums/AlertSeverity.java
@@ -0,0 +1,9 @@
+package com.fitgeek.alert.entity.enums;
+
+public enum AlertSeverity {
+
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
---
+++ alert-service/src/main/java/com/fitgeek/alert/entity/enums/AlertType.java
@@ -0,0 +1,8 @@
+package com.fitgeek.alert.entity.enums;
+
+public enum AlertType {
+
+    HIGH_TEMPERATURE,
+    LOW_BATTERY,
+    DEVICE_OFFLINE
+}
