7309fba98ae63b9d37996cbd6763bc5b556f63d3
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-14 19:20:23 +0200

    fix : monitoring-service: updated repository to add existsByDeviceId methode

--- monitoring-service/src/main/java/com/fitgeek/monitoring/repository/DeviceMonitoringRepository.java
+++ monitoring-service/src/main/java/com/fitgeek/monitoring/repository/DeviceMonitoringRepository.java
@@ -1,9 +1,11 @@
 package com.fitgeek.monitoring.repository;
 
-import com.fitgeek.monitoring.entity.DeviceMonitoring;
+import com.fitgeek.monitoring.entity.MonitoredDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface DeviceMonitoringRepository extends JpaRepository<DeviceMonitoring, UUID> {
+public interface DeviceMonitoringRepository extends JpaRepository<MonitoredDevice, UUID> {
+
+    Boolean existsByDeviceId(UUID deviceId);
 }
