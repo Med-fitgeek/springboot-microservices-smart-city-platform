26d9ecdc75f939f00252a143d878d57c81ed67a8
Med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2026-06-15 17:28:32 +0200

    feat : alert-service/monitoring-service: added security and OAuth2 features

--- alert-service/src/main/java/com/fitgeek/alert/config/SecurityConfig.java
+++ alert-service/src/main/java/com/fitgeek/alert/config/SecurityConfig.java
@@ -1,8 +1,7 @@
-package com.fitgeek.device.config;
+package com.fitgeek.alert.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
